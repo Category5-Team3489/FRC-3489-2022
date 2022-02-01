a1d6d99468a2a0e78d89954bdad1f8c08cb52c0d
3489ProgrammingFleet <98435354+3489ProgrammingFleet@users.noreply.github.com>
2022-01-31 19:27:10 -0500

    added the code for the shooter handler

--- src/main/java/frc/robot/ShooterHandler.java
+++ src/main/java/frc/robot/ShooterHandler.java
@@ -1,5 +1,32 @@
 package frc.robot;
 
 public class ShooterHandler {
-    
+    ComponentsContainer container;
+    public ShooterHandler(){
+
+    }
+    public void ShootHigh(){
+        container.shooterLeft.set(Constants.ShooterHighSpeed);
+        container.shooterRight.set(-Constants.ShooterHighSpeed);
+    }
+    public void ShootLow(){
+        container.shooterLeft.set(Constants.ShooterLowSpeed);
+        container.shooterRight.set(-Constants.ShooterLowSpeed);
+    }
+    public void stopShooting(){
+        container.shooterLeft.stopMotor();
+        container.shooterRight.stopMotor();
+    }
+    public void AdjustShootSpeed(double value){
+        double CurrentSpeed = container.shooterLeft.get();
+        if (CurrentSpeed + value <= 1){
+            container.shooterLeft.set(CurrentSpeed + value);
+            container.shooterRight.set(CurrentSpeed + value); 
+            
+        }      
+        if (CurrentSpeed - value >= -1)  {
+            container.shooterLeft.set(CurrentSpeed - value);
+            container.shooterRight.set(CurrentSpeed - value);    
+        }
+    }
 }
