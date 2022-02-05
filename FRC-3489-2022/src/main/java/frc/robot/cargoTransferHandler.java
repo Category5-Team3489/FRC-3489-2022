a2cdbe73d5eed32494e3b9879629e379a1d13872
3489ProgrammingFleet <98435354+3489ProgrammingFleet@users.noreply.github.com>
2022-02-05 13:17:08 -0500

    Update cargoTransferHandler.java

--- FRC-3489-2022/src/main/java/frc/robot/cargoTransferHandler.java
+++ FRC-3489-2022/src/main/java/frc/robot/cargoTransferHandler.java
@@ -1,3 +1,13 @@
 public class cargoTransferHandler {
-    
+    ComponentsContainer container;
+    Constants constants;
+    public void transferUp(){
+        container.cargoMoverMotor.set(constants.intakeSpeed);
+    }
+    public void transferDown(){
+        container.cargoMoverMotor.set(-constants.intakeSpeed);
+    }
+    public void transferStop(){
+        container.cargoMoverMotor.stopMotor();
+    }
 }
