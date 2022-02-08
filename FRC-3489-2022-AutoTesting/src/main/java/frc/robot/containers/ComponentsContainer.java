a20a41419ac524b8b6bd1bc789d125082098e80d
3489ProgrammingFleet <98435354+3489ProgrammingFleet@users.noreply.github.com>
2022-02-08 17:44:01 -0500

    Drive code

--- FRC-3489-2022-AutoTesting/src/main/java/frc/robot/containers/ComponentsContainer.java
+++ FRC-3489-2022-AutoTesting/src/main/java/frc/robot/containers/ComponentsContainer.java
@@ -4,11 +4,20 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public final class ComponentsContainer {
     
-    public WPI_TalonSRX leftTestMotor = new WPI_TalonSRX(6);
-    public WPI_TalonSRX rightTestMotor = new WPI_TalonSRX(9);
+    public WPI_TalonSRX leftTestMotor;// = new WPI_TalonSRX(6);
+    public WPI_TalonSRX rightTestMotor;// = new WPI_TalonSRX(9);
+
+    public WPI_TalonSRX leftFrontDriveMotor = new WPI_TalonSRX(1);
+    public WPI_TalonSRX rightFrontDriveMotor = new WPI_TalonSRX(2);
+    public WPI_TalonSRX leftFollowerDriveMotor = new WPI_TalonSRX(3);
+    public WPI_TalonSRX rightFollowerDriveMotor = new WPI_TalonSRX(4);
 
     public ComponentsContainer() {
-        leftTestMotor.setSafetyEnabled(false);
-        rightTestMotor.setSafetyEnabled(false);
+        //leftTestMotor.setSafetyEnabled(false);
+        //rightTestMotor.setSafetyEnabled(false);
+
+        rightFrontDriveMotor.setInverted(true);
+        leftFollowerDriveMotor.follow(leftFrontDriveMotor);
+        rightFollowerDriveMotor.follow(rightFrontDriveMotor);
     }
 }
--- FRC-3489-2022-AutoTesting/src/main/java/frc/robot/handlers/DriveHandler.java
+++ FRC-3489-2022-AutoTesting/src/main/java/frc/robot/handlers/DriveHandler.java
@@ -1,7 +1,41 @@
 package frc.robot.handlers;
 
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import frc.robot.framework.RobotHandler;
 
 public class DriveHandler extends RobotHandler {
+    
+    private Joystick leftDriveJoystick = new Joystick(0);
+    private Joystick rightDriveJoystick = new Joystick(1);
+
+    private DifferentialDrive drive;
+
+    private boolean frontSwitched = false;
+
+    private boolean shouldSwitchFront() {
+        return leftDriveJoystick.getRawButtonPressed(13) || rightDriveJoystick.getRawButtonPressed(13);
+    }
+
+    @Override
+    public void teleopInit() {
+        drive = new DifferentialDrive(components.leftFrontDriveMotor, components.rightFrontDriveMotor);
+    }
+
+    @Override
+    public void teleopPeriodic() {
+        if (shouldSwitchFront())
+            frontSwitched = !frontSwitched;
+        double leftY = leftDriveJoystick.getY();
+        double rightY = rightDriveJoystick.getY();
+        double leftSpeed = 0;
+        double rightSpeed = 0;
+        if (Math.abs(leftY) >= 0.1) leftSpeed = leftY;
+        if (Math.abs(rightY) >= 0.1) rightSpeed = rightY;
+        if (frontSwitched)
+            drive.tankDrive(leftSpeed, rightSpeed);
+        else
+            drive.tankDrive(-rightSpeed, -leftSpeed);
+    }
     
 }
