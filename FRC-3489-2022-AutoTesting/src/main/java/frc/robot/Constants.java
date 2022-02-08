1a6bd61275d0ac46dbdd159249a9ea77949f2cc1
3489ProgrammingFleet <98435354+3489ProgrammingFleet@users.noreply.github.com>
2022-02-08 18:01:41 -0500

    improvements

---
+++ FRC-3489-2022-AutoTesting/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,9 @@
+package frc.robot;
+
+public final class Constants {
+
+    public static final boolean HasCameras = false;
+    public static final boolean IsRobotInABox = false;
+    public static final boolean SafetiesEnabled = true;
+
+}
--- FRC-3489-2022-AutoTesting/src/main/java/frc/robot/containers/ComponentsContainer.java
+++ FRC-3489-2022-AutoTesting/src/main/java/frc/robot/containers/ComponentsContainer.java
@@ -4,30 +4,50 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.Constants;
 
 public final class ComponentsContainer {
     
-    public WPI_TalonSRX leftTestMotor;// = new WPI_TalonSRX(6);
-    public WPI_TalonSRX rightTestMotor;// = new WPI_TalonSRX(9);
+    public WPI_TalonSRX leftTestMotor;
+    public WPI_TalonSRX rightTestMotor;
 
     // Joysticks
-    public Joystick leftDriveJoystick = new Joystick(0);
-    public Joystick rightDriveJoystick = new Joystick(1);
+    public Joystick leftDriveJoystick;
+    public Joystick rightDriveJoystick;
 
     // Drive
-    public WPI_TalonSRX leftFrontDriveMotor = new WPI_TalonSRX(1);
-    public WPI_TalonSRX rightFrontDriveMotor = new WPI_TalonSRX(2);
-    public WPI_TalonSRX leftFollowerDriveMotor = new WPI_TalonSRX(3);
-    public WPI_TalonSRX rightFollowerDriveMotor = new WPI_TalonSRX(4);
+    public WPI_TalonSRX leftFrontDriveMotor;
+    public WPI_TalonSRX rightFrontDriveMotor;
+    public WPI_TalonSRX leftFollowerDriveMotor;
+    public WPI_TalonSRX rightFollowerDriveMotor;
     public DifferentialDrive drive;
 
     public ComponentsContainer() {
-        //leftTestMotor.setSafetyEnabled(false);
-        //rightTestMotor.setSafetyEnabled(false);
+
+        if (Constants.IsRobotInABox) {
+            setSafeties(leftTestMotor = new WPI_TalonSRX(6));
+            setSafeties(rightTestMotor = new WPI_TalonSRX(9));
+            return;
+        }
+
+        leftDriveJoystick = new Joystick(0);
+        rightDriveJoystick = new Joystick(1);
+
+        setSafeties(leftFrontDriveMotor = new WPI_TalonSRX(1));
+        setSafeties(rightFrontDriveMotor = new WPI_TalonSRX(2));
+        setSafeties(leftFollowerDriveMotor = new WPI_TalonSRX(3));
+        setSafeties(rightFollowerDriveMotor = new WPI_TalonSRX(4));
 
         rightFrontDriveMotor.setInverted(true);
         leftFollowerDriveMotor.follow(leftFrontDriveMotor);
         rightFollowerDriveMotor.follow(rightFrontDriveMotor);
-        drive = new DifferentialDrive(leftFrontDriveMotor, rightFrontDriveMotor);
+        setSafeties(drive = new DifferentialDrive(leftFrontDriveMotor, rightFrontDriveMotor));
+    }
+
+    private void setSafeties(WPI_TalonSRX motor) {
+        motor.setSafetyEnabled(Constants.SafetiesEnabled);
+    }
+    private void setSafeties(DifferentialDrive drive) {
+        drive.setSafetyEnabled(Constants.SafetiesEnabled);
     }
 }
