dc494909ce6a6d274ba63bd9e478e4ed30cb10e4
3489ProgrammingFleet <98435354+3489ProgrammingFleet@users.noreply.github.com>
2022-02-09 17:47:10 -0500

    New Auto 1
    Mackenzie

---
+++ FRC-3489-2022/src/main/java/frc/robot/AutoFolder/Auto1.2.java
@@ -0,0 +1,67 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+//Move Forward, Turn Left, Move Forward
+
+public class Auto1 {
+    private ComponentsContainer components;
+    private DifferentialDrive differentialDrive;
+    private DriveHandler driveHandler;
+    private Constants constants;
+    private int currentStep = 1;
+
+    private double getEncoderPositionAbs(){
+        return Math.abs(components.frontRightDrive.getSelectedSensorPosition());
+    }
+   
+    public void init(){
+        resetEncoderPosition();
+    }
+
+    private void resetEncoderPosition(){
+        components.frontLeftDrive.setSelectedSensorPosition(0);
+    }
+
+    //sequence
+    public void auto1(){
+        switch(currentStep){
+            case 1:
+            driveForward();
+            break;
+            case 2:
+            turnLeft();
+            break;
+            case 3:
+            driveForward();
+            break;
+        }
+
+    }
+    //Drive Forward
+    private void driveForward(){
+        if (getEncoderPositionAbs()< constants.auto1ForwardEncoderClicks){
+            driveHandler.tankDrive(0.5, 0.5);}
+        else{
+            currentStep++;
+            driveHandler.stop();
+            resetEncoderPosition();
+        }
+
+    }
+        //Turn Left
+    private void turnLeft(){
+        if (getEncoderPositionAbs()<constants.auto1TurnLeft){
+            driveHandler.tankDrive(0.4, 0.7);
+        }
+        else{
+            currentStep++;
+            driveHandler.stop();
+            resetEncoderPosition();
+
+        }
+      
+    }
+   
+}
+
