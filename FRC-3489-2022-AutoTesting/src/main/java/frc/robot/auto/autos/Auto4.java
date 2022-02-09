a755e7d68bfbc8388b430f05c60d0a47e43d8caa
3489ProgrammingFleet <98435354+3489ProgrammingFleet@users.noreply.github.com>
2022-02-08 19:28:46 -0500

    Comment auto 4

--- FRC-3489-2022-AutoTesting/src/main/java/frc/robot/auto/autos/Auto4.java
+++ FRC-3489-2022-AutoTesting/src/main/java/frc/robot/auto/autos/Auto4.java
@@ -12,14 +12,15 @@ public class Auto4 extends AutoBuilder {
 
         first
         .concurrently(
-            shoot(0.9, 4),
-            cargoTransfer(0.5, 5 * Auto.CargoTransferClicksPerBall)
+            shoot(0.9, 3), // Set shooter to 90% for 3 seconds
+            pause(1) // Pause for 1 second before shooting ball
+                .cargoTransfer(0.5, 5 * Auto.CargoTransferClicksPerBall) // Move cargo transfer at 50% for 5 ball lengths to shoot ball
         )
-        .pause(1)
-        .drive(0.65, 8 * Auto.DriveClicksPerFoot)
-        .pause(1)
-        .turn(0.4, 30)
-        .drive(0.65, 6 * Auto.DriveClicksPerFoot);
+        .pause(1) // Wait one sec after shooting
+        .drive(0.65, 8 * Auto.DriveClicksPerFoot) // Drive at 65% for 8 feet
+        .pause(1) // Wait one sec for ball to roll away
+        .turn(0.4, 30) // Turn at 40% speed 30 degrees to the left
+        .drive(0.65, 6 * Auto.DriveClicksPerFoot); // Drive at 65% for 6 feet
         
         return first;
     }
