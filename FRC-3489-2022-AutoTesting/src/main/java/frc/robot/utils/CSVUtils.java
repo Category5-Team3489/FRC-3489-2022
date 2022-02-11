ba7d470e5535cfa665685800900fedc482b684b5
Connor <61102529+Metater@users.noreply.github.com>
2022-02-11 15:58:14 -0500

    CSV Utils

--- FRC-3489-2022-AutoTesting/src/main/java/frc/robot/utils/CSVUtils.java
+++ FRC-3489-2022-AutoTesting/src/main/java/frc/robot/utils/CSVUtils.java
@@ -1,5 +1,16 @@
 package frc.robot.handlers;
 
 public static class CSVUtils {
-    public static Map<String, String>
+
+    private static Map<String, String> files = new HashMap<String, String>();
+    // use files.putIfAbsent(key, val);
+
+    public static void setColumns(String csv, String... columns) {
+        String columnsRow = "";
+        for (int i = 0; i < columns.length - 2; i++) {
+            columnsRow += columns[i] + ",";
+        }
+        columnsRow += columns[columns.length - 1];
+        files.putIfAbsent(csv, columnsRow + "\n");
+    }
 }
