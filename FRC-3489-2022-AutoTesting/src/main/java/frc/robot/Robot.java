bc267db173313f16357d12ab153905a4ee124031
3489ProgrammingFleet <98435354+3489ProgrammingFleet@users.noreply.github.com>
2022-02-28 18:23:05 -0500

    Use TODO.txt instead

--- FRC-3489-2022-AutoTesting/src/main/java/frc/robot/Robot.java
+++ FRC-3489-2022-AutoTesting/src/main/java/frc/robot/Robot.java
@@ -11,33 +11,6 @@ public class Robot extends TimedRobot {
 
   private RobotManager robotManager;
 
-  // PRIORITY
-  // TODO Climber
-
-  // LATER
-  // TODO Logger handler
-  // TODO Button debouncer utils
-  // TODO Shooter velocity control with PID
-  // TODO Improve slew rate limiting for drive code, may only want to use abrupt breaking
-  // TODO Fix 2 drive docs
-
-  // MAYBE???
-  // TODO Auto docs
-  // TODO Get class name method in RobotReferences? for logger
-
-  // Useful info
-  // look at Ziegler–Nichols tuning
-  // https://docs.wpilib.org/en/stable/docs/software/roborio-info/roborio-ftp.html
-  // https://github.com/rawgraphs/rawgraphs-app
-
-  // OLD
-  // TODO Conditional integration? May be better than clamping??
-  // TODO Clamp integration when D is above threshold? May cause jerk when I starts influencing
-  // TODO Integrator anti-windup?
-  // PID Constant interpolation?
-  // PID X set speed, Y voltage drop
-  // TODO Revaluate PID constants when accel stops?
-
   @Override
   public void robotInit() {
     robotManager = new RobotManager(this);
