bc675d1418a424d70bc971eaa04f9dee94b51164
3489ProgrammingFleet <98435354+3489ProgrammingFleet@users.noreply.github.com>
2022-03-02 17:57:11 -0500

    Auto 6
    mackenzie

--- FRC-3489-2022-AutoTesting/src/main/java/frc/robot/auto/autos/Auto6.java
+++ FRC-3489-2022-AutoTesting/src/main/java/frc/robot/auto/autos/Auto6.java
@@ -11,15 +11,15 @@ public class Auto6 extends AutoBuilder {
         AutoInstruction first = first();
 
         first
-        .concurrently(
-            shoot(0.65, 3), //Set shooter to 65% for 3 seconds
+        .concurrently( //shoot high
+            shoot(0.5, 3), //Set shooter to 50% for 3 seconds
             pause(1) //Pause for 1 second before shooting ball
                 .cargoTransfer(0.5,5 * Constants.ClicksPerCargoLength) //Move cargo transfer at 50% for 5 ball lengths to shoot ball
         )
         .pause(1) //Wait one second
-        .concurrently(
+        .concurrently( //drive forward while intaking
             drive(0.65, 4.5 * Constants.DriveClicksPerFoot), //Drive at 65% for 4.5 feet
-            intake(0.6, 10) //intake at 60% speed for 10 seconde
+            intake(0.6, 6) //intake at 60% speed for 6 seconde
         )
         .pause(1) //wait for one second
         .turn(.5, -100) //turn right 100 degrees at 50% speed
