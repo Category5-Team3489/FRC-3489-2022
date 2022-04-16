4ee62a8459f17bd37c977b5ab90a2c390df31c64
3489ProgrammingFleet <98435354+3489ProgrammingFleet@users.noreply.github.com>
2022-04-16 15:25:12 -0400

    3 and 5 ball auto
    mackenzie

---
+++ FRC-3489-2022-AutoTesting/src/main/java/frc/robot/auto/autos/Auto3BallAim.java
@@ -0,0 +1,61 @@
+package frc.robot.auto.autos;
+
+import frc.robot.Constants;
+import frc.robot.auto.framework.AutoBuilder;
+import frc.robot.auto.framework.AutoInstruction;
+import frc.robot.types.LimelightMode;
+
+public class AutoQuickPickupAim extends AutoBuilder {
+
+    @Override
+    public AutoInstruction build() {
+        AutoInstruction first = first();
+
+        // shoot
+        // drive out of tarmac
+        first
+        .onInitialized(() -> {
+            limelightHandler.setLimelightMode(LimelightMode.AutoAim);
+        })
+        .concurrently(
+            shoot(0.5, 4),
+            pause(3)
+                .cargoTransfer(0.5, 5 * Constants.CargoTransfer.ClicksPerCargoLength)
+        )//shoot high once
+        .concurrently(
+            driveSeconds(0.65, 1.35 * 0.75), // 1.5
+            intake(2)
+        )//drive foward and intake
+        .cargoTransfer(0.5, 0.5 * Constants.CargoTransfer.ClicksPerCargoLength)
+        .concurrently(
+            driveSeconds(-0.65, (1 * 0.75) - 0.1)
+        )//drive back to tarmac line
+        .blank(false)
+        .periodically(() -> {
+            return driveHandler.autoAim();
+        })//set mode to auto aim
+        .onCompleted(() -> {
+            limelightHandler.setLimelightMode(LimelightMode.Driver);
+        })//set mode to driver
+        .concurrently(
+            shoot(0.5, 4),//shoot high once
+            pause(2)
+            .cargoTransfer(0.5, 10 * Constants.CargoTransfer.ClicksPerCargoLength)
+            .completeOn(getTrigger("stop")),
+            pause(4)
+                .onCompleted(setTrigger("stop"))
+            /*
+            .onCompleted(() -> {
+                cargoTransferHandler.set(-0.5);
+            })
+            .pause(3)
+            .onCompleted(() -> {
+                cargoTransferHandler.set(0);
+            })
+            */
+        );
+
+        return first;
+    }
+    
+}
