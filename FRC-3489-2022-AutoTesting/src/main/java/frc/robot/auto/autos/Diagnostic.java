1bc34366b99ce34652ee57f07d6cf77fffe40cb5
3489ProgrammingFleet <98435354+3489ProgrammingFleet@users.noreply.github.com>
2022-04-18 18:41:19 -0400

    Diagnostic stuff

--- FRC-3489-2022-AutoTesting/src/main/java/frc/robot/auto/autos/Diagnostic.java
+++ FRC-3489-2022-AutoTesting/src/main/java/frc/robot/auto/autos/Diagnostic.java
@@ -1,5 +1,11 @@
 package frc.robot.auto.autos;
 
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.Constants;
 import frc.robot.auto.framework.AutoBuilder;
 import frc.robot.auto.framework.AutoInstruction;
 
@@ -8,6 +14,11 @@ public class Diagnostic extends AutoBuilder {
     @Override
     public AutoInstruction build() {
         AutoInstruction first = first();
+
+        first
+        .periodically(() -> {
+
+        });
 
         first
         .concurrently(diagnostic.intakeError())
@@ -21,7 +32,62 @@ public class Diagnostic extends AutoBuilder {
         .concurrently(diagnostic.telescopeError())
         .concurrently(diagnostic.navxError())
         .concurrently(diagnostic.intakeLaserError());
-    
+
+        AutoInstruction testIntake = testMotor(first,
+        () -> { // start motor
+            components.intakeMotor.set(Constants.Speeds.ForwardIntakeMotorSpeed);
+        },
+        () -> { // stop motor
+            components.intakeMotor.stopMotor();
+        },
+        4, // test time
+        () -> { // get motor speed
+            return components.intakeMotor.getSelectedSensorVelocity();
+        },
+        () -> { // get motor current
+            return components.intakeMotor.getStatorCurrent();
+        });
+
+        // print out temperatures for motors
+
+        // make sure it isnt triggered instantly
+        // for laser sensor test, print out to trigger sensor, then wait for 10 sec, if they dont trigger it print test failed
+
+        // for navx test just print out all of the values for it at an instant
+
+        // print out time for climber telescope moving up and down, and compare to baseline
+
+        return first;
+    }
+
+    double avgSpeed = 0.0;
+    double lastSpeed = 0.0;
+    double maxSpeed = 0.0;
+
+    double avgCurrent = 0.0;
+    double lastCurrent = 0.0;
+    double maxCurrent = 0.0;
+
+    private AutoInstruction testMotor(AutoInstruction first, Runnable startMotor, Runnable stopMotor, double time, Supplier<Double> getSpeed, Supplier<Double> getCurrent) {
+        
+        Timer timer = new Timer();
+
+        return first
+        .onInitialized(() -> {
+            timer.start();
+        })
+        .periodically(() -> {
+            if (timer.hasElapsed(time)) {
+                lastSpeed = getSpeed.get();
+                lastCurrent = getCurrent.get();
+                stopMotor.run();
+                return true;
+            }
+            else {
+                startMotor.run();
+            }
+            return false;
+        });
     }
         
 }
