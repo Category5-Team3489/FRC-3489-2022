6620fe80e51f82246cd49e4a401d45965f89c8a5
3489ProgrammingFleet <98435354+3489ProgrammingFleet@users.noreply.github.com>
2022-04-21 10:14:38 -0500

    New auto

--- FRC-3489-2022-AutoTesting/src/main/java/frc/robot/auto/autos/AutoNew.java
+++ FRC-3489-2022-AutoTesting/src/main/java/frc/robot/auto/autos/AutoNew.java
@@ -1,7 +1,9 @@
 package frc.robot.auto.autos;
 
+import frc.robot.Constants;
 import frc.robot.auto.framework.AutoBuilder;
 import frc.robot.auto.framework.AutoInstruction;
+import frc.robot.types.DriveState;
 
 public class AutoNew extends AutoBuilder {
 
@@ -9,7 +11,31 @@ public class AutoNew extends AutoBuilder {
     public AutoInstruction build() {
         AutoInstruction first = first();
 
-        
+        first.onInitialized(() -> {
+            driveHandler.toggleToAim();
+        })
+        .blank(false)
+        .periodically(() -> {
+            return driveHandler.getDriveState() == DriveState.Driving;
+        })
+        .concurrently(
+            driveSeconds(0.65, 1.35 * 0.75), // 1.5
+            intake(2)
+        )
+        .concurrently(
+            cargoTransfer(0.5, 0.5 * Constants.CargoTransfer.ClicksPerCargoLength),
+            driveSeconds(-0.65, 1 * 0.75)
+        )
+        .onCompleted(() -> {
+            driveHandler.toggleToAim();
+        })
+        .blank(false)
+        .periodically(() -> {
+            return driveHandler.getDriveState() == DriveState.Driving;
+        })
+        .onCompleted(() -> {
+            driveHandler.toggleToDrive();
+        });
 
         return first;
     }
