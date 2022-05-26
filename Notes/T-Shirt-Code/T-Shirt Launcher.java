347cf6f8f9b2b68b75c9c3bd6042f16a1ad1a56a
3489ProgrammingFleet <98435354+3489ProgrammingFleet@users.noreply.github.com>
2022-05-26 17:33:12 -0400

    Create T-Shirt Launcher.java

---
+++ Notes/T-Shirt-Code/T-Shirt Launcher.java
@@ -0,0 +1,17 @@
+Digital
+1 Pressure Switch Channel
+4 Compressor Relay Channel
+
+Solenoids
+1 Fire One
+2 Fire Two
+3 Fire Three
+4 Fire Four
+5 Block Air Flow
+6/7 Raise and Lower(Double Solenoid)
+
+PWM
+9 Back Right Wheel
+2 Back Left Wheel
+1 Front Right Wheel
+10 Back Left Wheel
