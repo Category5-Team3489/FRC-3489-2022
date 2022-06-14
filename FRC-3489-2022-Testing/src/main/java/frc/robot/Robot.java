5e35c797af7bf3c33d9a074856007956a269dc27
3489ProgrammingFleet <98435354+3489ProgrammingFleet@users.noreply.github.com>
2022-06-14 18:16:33 -0400

    Add testing project for testing new camera

---
+++ FRC-3489-2022-Testing/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,79 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.UsbCamera;
+import edu.wpi.first.cscore.VideoSink;
+import edu.wpi.first.cscore.VideoSource.ConnectionStrategy;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+    UsbCamera cameraA = CameraServer.startAutomaticCapture(0);
+    UsbCamera cameraB = CameraServer.startAutomaticCapture(1);
+    VideoSink server = CameraServer.getServer();
+
+    cameraA.setConnectionStrategy(ConnectionStrategy.kKeepOpen);
+    //camera.setResolution(160, 120);
+    cameraA.setResolution(256, 256);
+    cameraA.setFPS(15);
+
+    ShuffleboardTab tab = Shuffleboard.getTab("Testing");
+
+    tab
+    .add(server.getSource())
+    .withWidget(BuiltInWidgets.kCameraStream)
+    .withSize(5, 5)
+    .withPosition(0, 0);
+  }
+
+  @Override
+  public void robotPeriodic() {}
+
+  @Override
+  public void autonomousInit() {}
+
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {}
+
+  @Override
+  public void teleopPeriodic() {}
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void testInit() {}
+
+  @Override
+  public void testPeriodic() {}
+
+  @Override
+  public void simulationInit() {}
+
+  @Override
+  public void simulationPeriodic() {}
+}
